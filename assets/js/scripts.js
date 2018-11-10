8892df284d353fe191c20c9ae7ef08e8ce6918b2
Chris Quinn <10xoxr@gmail.com>
2018-11-10 14:57:55 +0000

    Added planets to 'planets' array. Changed D3 animation timing.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,16 +11,51 @@ var w = 800,
         {
             planName: "Venus",
             orbitRadius: 80,
-            planRadius: 14.2,
+            planRadius: 9,
             orbitSpeed: 69,
             color: "#E89624"
         },
         {
             planName: "Earth",
             orbitRadius: 115,
-            planRadius: 15,
+            planRadius: 10,
             orbitSpeed: 50,
             color: "#518E87"
+        },
+        {
+            planName: "Mars",
+            orbitRadius: 150,
+            planRadius: 6,
+            orbitSpeed: 26.5,
+            color: "#964120"
+        },
+        {
+            planName: "Jupiter",
+            orbitRadius: 200,
+            planRadius: 30,
+            orbitSpeed: 4.15,
+            color: "#BBAC93"
+        },
+        {
+            planName: "Saturn",
+            orbitRadius: 265,
+            planRadius: 25,
+            orbitSpeed: 1.725,
+            color: "#A38E5F"
+        },
+        {
+            planName: "Uranus",
+            orbitRadius: 315,
+            planRadius: 16,
+            orbitSpeed: .595,
+            color: "#CDF3F6"
+        },
+        {
+            planName: "Neptune",
+            orbitRadius: 360,
+            planRadius: 15,
+            orbitSpeed: .305,
+            color: "#4270FD"
         }
     ];
 
@@ -60,6 +95,6 @@ d3.timer(function () {
     var delta = (Date.now() - startTime);
     svg.selectAll(".planName")
         .attr("transform", function (d) {
-            return "rotate(" + delta * d.orbitSpeed / 200 + ")";
+            return "rotate(" + delta * d.orbitSpeed / 175 + ")";
         });
 });
