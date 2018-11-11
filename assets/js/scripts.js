0bedf4be8e722099b477ab575d8ab9d3f8b70e55
Chris Quinn <10xoxr@gmail.com>
2018-11-11 20:42:27 +0000

    Began inserting DC.js chart functions.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,58 +5,45 @@ $(document).ready(function () {
     // On load, hide all sections aside from 'Home'.
     $(function () {
         $(".statistics, .exoplanets, .terminology, .methodology, .resources").hide();
-      
+
         // When each nav li is clicked, hide all sections.
         $("#navHome, #navStats, #navExo, #navTerms, #navMethod, #navResc, #cta").on("click", function () {
-          $(".home, .statistics, .exoplanets, .terminology, .methodology, .resources").hide();
-          
-        // Remove the 'selected' class from all li elements.
-          $("#navHome,#navStats,#navExo,#navTerms,#navMethod,#navResc").removeClass('selected');
+            $(".home, .statistics, .exoplanets, .terminology, .methodology, .resources").hide();
 
-        // If/else statements to check which li was clicked,
-        // apply 'selected' class only to that li, and fadeIn the corresponding section.
-          if ($(this).attr("id") == "navHome")
-          {
-            $("#navHome").addClass('selected');
-            $(".home").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "navStats")
-          {
-            $("#navStats").addClass('selected');
-            $(".statistics").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "navExo")
-          {
-            $("#navExo").addClass('selected');
-            $(".exoplanets").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "navTerms")
-          {
-            $("#navTerms").addClass('selected');
-            $(".terminology").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "navMethod")
-          {
-            $("#navMethod").addClass('selected');
-            $(".methodology").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "navResc")
-          {
-            $("#navResc").addClass('selected');
-            $(".resources").fadeIn(500);
-          }
-          else if ($(this).attr("id") == "cta")
-          {
-            $("#navStats").addClass('selected');
-            $(".statistics").fadeIn(500);
-          }
+            // Remove the 'selected' class from all li elements.
+            $("#navHome,#navStats,#navExo,#navTerms,#navMethod,#navResc").removeClass('selected');
+
+            // If/else statements to check which li was clicked,
+            // apply 'selected' class only to that li, and fadeIn the corresponding section.
+            if ($(this).attr("id") == "navHome") {
+                $("#navHome").addClass('selected');
+                $(".home").fadeIn(500);
+            } else if ($(this).attr("id") == "navStats") {
+                $("#navStats").addClass('selected');
+                $(".statistics").fadeIn(500);
+            } else if ($(this).attr("id") == "navExo") {
+                $("#navExo").addClass('selected');
+                $(".exoplanets").fadeIn(500);
+            } else if ($(this).attr("id") == "navTerms") {
+                $("#navTerms").addClass('selected');
+                $(".terminology").fadeIn(500);
+            } else if ($(this).attr("id") == "navMethod") {
+                $("#navMethod").addClass('selected');
+                $(".methodology").fadeIn(500);
+            } else if ($(this).attr("id") == "navResc") {
+                $("#navResc").addClass('selected');
+                $(".resources").fadeIn(500);
+            } else if ($(this).attr("id") == "cta") {
+                $("#navStats").addClass('selected');
+                $(".statistics").fadeIn(500);
+            }
         });
-      });
-    
+    });
+
     // Toggle the displayNone class to show/hide the navbar only when
     // the screen/window size is below 820px.
     $("#menu-toggle, #navHome, #navStats, #navExo, #navTerms, #navMethod, #navResc").on("click", function () {
-        if ($(window).width() <820) {
+        if ($(window).width() < 820) {
             $("#nav>ul").slideToggle('normal', function () {
                 $(this).css('display', '').toggleClass('displayNone');
             });
@@ -133,7 +120,7 @@ var w = 800,
         }
     ];
 
-// Create an SVG and attach it to the #planets div.
+// Create an SVG and attach it to the #planets section.
 // Use SVG viewBox to control the scaling of the SVG.
 var svg = d3.select("#planets")
     .append("svg")
@@ -149,14 +136,14 @@ svg.append("circle")
 //Append defs to store graphical objects.
 var defs = svg.append("defs");
 
-//Append a radialGradient element to the defs and give it a unique id.
+// Append a radialGradient element to the defs and give it a unique id.
 var radialGradient = defs.append("radialGradient")
     .attr("id", "radial-gradient")
     .attr("cx", "50%")
     .attr("cy", "50%")
     .attr("r", "50%");
 
-//Add colors to make the gradient appear like the Sun.
+// Add colors to make the gradient appear like the Sun.
 radialGradient.append("stop")
     .attr("offset", "0%")
     .attr("stop-color", "#FFF76B");
@@ -173,21 +160,24 @@ radialGradient.append("stop")
     .attr("offset", "100%")
     .attr("stop-color", "#FB8933");
 
-//Create a radial gradient for each of the planets.
+// Create a radial gradient for each of the planets.
 var planetGradients = svg.append("defs").selectAll("radialGradient")
     .data(planets)
     .enter()
     .append("radialGradient")
 
-    //Create a unique id per planet
+    // Create a unique id per planet.
     .attr("id", function (d) {
         return "gradient-" + d.planetName;
     })
+
+    // Offset the gradients to appear as though each planet.
+    // is being lit by the Sun,
     .attr("cx", "35%")
     .attr("cy", "50%")
     .attr("r", "60%");
 
-//Add colors to the gradient
+// Add colors to the gradient.
 planetGradients.append("stop")
     .attr("offset", "0%")
     .attr("stop-color", function (d) {
@@ -242,3 +232,127 @@ d3.timer(function () {
 
 // ------------------------------------- STATISTICS SECTION -------------------------------------
 
+queue()
+    .defer(d3.csv, "assets/data/planets.csv")
+    .await(makeGraphs);
+
+function makeGraphs(error, exoPlanetData) {
+    var ndx = crossfilter(exoPlanetData);
+
+    exoPlanetData.forEach(function (d) {
+        d.pl_disc = parseInt(d.pl_disc);
+        d.loc_rowid = parseInt(d.loc_rowid);
+    });
+
+    //exo_discovery_year(ndx);
+
+    observatory_location(ndx, "Ground", "#ground-based");
+    observatory_location(ndx, "Space", "#space-based");
+    observatory_location(ndx, "Multiple Locales", "#multiple");
+
+    dc.renderAll();
+};
+
+function observatory_location(ndx, location, element) {
+    var percentageByLocation = ndx.groupAll().reduce(
+        function (p, v) {
+            if (typeof v.loc_rowid == 'number') {
+                p.count++;
+                if (v.pl_locale === location) {
+                    p.locale++;
+                }
+            }
+            return p;
+        },
+        function (p, v) {
+            if (typeof v.loc_rowid == 'number') {
+                p.count--;
+                if (v.pl_locale === location) {
+                    p.locale--;
+                }
+            }
+            return p;
+        },
+        function () {
+            return {
+                count: 0,
+                locale: 0
+            };
+        }
+    );
+    dc.numberDisplay(element)
+        .formatNumber(d3.format(".2%"))
+        .valueAccessor(function (d) {
+            if (d.count == 0) {
+                return 0;
+            } else {
+                return (d.locale / d.count);
+            }
+        })
+        .group(percentageByLocation)
+        .transitionDuration(1000);
+};
+
+/*function exo_discovery_year(ndx) {
+
+    function rankByGender(dimension, rank) {
+        return dimension.group().reduce(
+            function (p, v) {
+                p.total++;
+                if (v.rank == rank) {
+                    p.match++;
+                }
+                return p;
+            },
+            function (p, v) {
+                p.total--;
+                if (v.rank == rank) {
+                    p.match--;
+                }
+                return p;
+            },
+            function () {
+                return {
+                    total: 0,
+                    match: 0
+                };
+            }
+        );
+    };
+
+    var dim = ndx.dimension(dc.pluck("pl_disc"));
+    var profByGender = rankByGender(dim, "Prof");
+    var asstProfByGender = rankByGender(dim, "AsstProf");
+    var assocProfByGender = rankByGender(dim, "AssocProf");
+
+    dc.barChart("#rank-distribution")
+        .width(350)
+        .height(250)
+        .gap(10)
+        //.useViewBoxResizing(true)
+        .dimension(dim)
+        .group(profByGender, "Prof")
+        .stack(asstProfByGender, "Asst Prof")
+        .stack(assocProfByGender, "Assoc Prof")
+        .valueAccessor(function (d) {
+            if (d.value.total > 0) {
+                return (d.value.match / d.value.total).toFixed(3) * 100;
+            } else {
+                return 0;
+            }
+        })
+        .transitionDuration(500)
+        .transitionDelay(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .legend(dc.legend().x(270).y(20).itemHeight(15).gap(15))
+        .margins({
+            top: 10,
+            right: 100,
+            bottom: 30,
+            left: 30
+        })
+        .xAxisLabel("Gender")
+        .yAxisLabel("% Staff");
+
+};*/
